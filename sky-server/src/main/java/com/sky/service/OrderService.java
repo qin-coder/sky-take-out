862c23ec6ae2b499fe6627099ba1f9af6589e048
Xuwei <qinxuwei15@gmail.com>
2025-09-20 20:18:03 +0200

    check history orders on user-end

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,9 +1,12 @@
 package com.sky.service;
 
 import com.sky.dto.OrdersSubmitDTO;
+import com.sky.result.PageResult;
 import com.sky.vo.OrderSubmitVO;
 
 public interface OrderService {
 
     OrderSubmitVO submitOrder(OrdersSubmitDTO ordersSubmitDTO);
+
+    PageResult pageQuery4User(int page, int pageSize, Integer status);
 }
