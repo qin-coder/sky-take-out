ed7855a3a545796bb8a900b1b4dc0d964571be01
Xuwei <qinxuwei15@gmail.com>
2025-09-21 19:06:01 +0200

    search orders in Merchant-side

--- sky-server/src/main/java/com/sky/service/OrderService.java
+++ sky-server/src/main/java/com/sky/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.sky.service;
 
+import com.sky.dto.OrdersPageQueryDTO;
 import com.sky.dto.OrdersSubmitDTO;
 import com.sky.result.PageResult;
 import com.sky.vo.OrderSubmitVO;
@@ -16,4 +17,6 @@ public interface OrderService {
     void userCancelById(Long id);
 
     void repetition(Long id);
+
+    PageResult conditionSearch(OrdersPageQueryDTO ordersPageQueryDTO);
 }
