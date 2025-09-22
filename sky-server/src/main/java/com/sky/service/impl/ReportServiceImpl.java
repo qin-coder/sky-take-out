0efbd122077532001649b6034f707ff2ca54aac9
Xuwei <qinxuwei15@gmail.com>
2025-09-22 18:30:00 +0200

    Merchant-side revenue statistics function

---
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -0,0 +1,66 @@
+package com.sky.service.impl;
+
+import com.sky.entity.Orders;
+import com.sky.mapper.OrderMapper;
+import com.sky.service.ReportService;
+import com.sky.vo.TurnoverReportVO;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@Slf4j
+public class ReportServiceImpl implements ReportService {
+    @Autowired
+    private OrderMapper orderMapper;
+
+    /**
+     * 营业额统计
+     *
+     * @param begin
+     * @param end
+     * @return
+     */
+    public TurnoverReportVO getTurnoverStatistics(LocalDate begin,
+                                                  LocalDate end) {
+        List<LocalDate> dateList = new ArrayList<>(); //存放从begin
+        // 到end范围内每天的日期
+        dateList.add(begin);
+        while (!begin.equals(end)) {
+            //日期激素按，计算指定 日期后一天对应的日期
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+        StringUtils.join(dateList, ",");
+
+        //获取营业额数据
+        List<Double> turnoverList = new ArrayList<>();
+        for (LocalDate date : dateList) {
+            //获取指定日期的营业额数据,状态为已完成的订单金额合计
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);
+            Map map = new HashMap();
+            map.put("begin", beginTime);
+            map.put("end", endTime);
+            map.put("status", Orders.COMPLETED);
+            Double turnover = orderMapper.sumByMap(map);
+            turnoverList.add(turnover == null ? 0.0 : turnover);
+
+        }
+        //组装返回结果
+        return TurnoverReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .turnoverList(StringUtils.join(turnoverList, ","))
+                .build();
+    }
+}
