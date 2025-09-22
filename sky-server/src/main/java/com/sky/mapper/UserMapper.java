4f077de8fd305856602b141ea4bf890a5205f024
Xuwei <qinxuwei15@gmail.com>
2025-09-22 18:50:44 +0200

    Merchant-side user data statistics function

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,10 +4,19 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
     @Select("select * from user where openid = #{openid}")
     User getByOpenid (String openid);
 
     void insert(User user);
+
+    /**
+     * 根据动态条件统计用户数
+     * @param map
+     * @return
+     */
+    Integer countByMap(Map map);
 }
